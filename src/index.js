693938e634df75e51703718152e69e2ec72f609a
trunghaiii <haitran022001@gmail.com>
2023-04-19 21:58:53 -0400

    adding loading bar when calling api with Nprogress

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'nprogress/nprogress.css'
 
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
--- src/utils/axiosCustomize.js
+++ src/utils/axiosCustomize.js
@@ -1,4 +1,7 @@
 import axios from "axios"
+import NProgress from 'nprogress'
+
+
 
 const instance = axios.create({
     baseURL: 'http://localhost:8081/'
@@ -7,6 +10,7 @@ const instance = axios.create({
 // Add a request interceptor
 instance.interceptors.request.use(function (config) {
     // Do something before request is sent
+    NProgress.start();
     return config;
 }, function (error) {
     // Do something with request error
@@ -17,10 +21,12 @@ instance.interceptors.request.use(function (config) {
 instance.interceptors.response.use(function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
+    NProgress.done();
     return response && response.data ? response.data : response;
 }, function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
+    NProgress.done();
     return error && error.response && error.response.data ?
         error.response.data : Promise.reject(error);
 });
